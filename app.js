3ef674acaf5c060bd2b814e52d9581c1a221e212
abiya30 <abiya300520@gmail.com>
2021-01-19 13:38:45 +0530

    added console log

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+	console.log('<html><head><title>Test Server</title></head><body><p>Hello World!!!</p></body></html>')
   res.send('Hello World!ii')
 })
 
