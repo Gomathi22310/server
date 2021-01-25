c1883a33179839d6960021129e5f46ce4339dcd3
abiya30 <abiya300520@gmail.com>
2021-01-26 01:05:40 +0530

    added ejs template

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
-
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
+app.use(express.static('./public'))
 app.get('/', (req, res) => {
-	console.log('<html><head><title>Test Server</title></head><body><p>Hello World!!!</p></body></html>')
-  res.send('Hello World!ii')
+	res.render('home');
 })
 
 app.listen(port, () => {
